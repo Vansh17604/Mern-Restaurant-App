b93664a35ad5dd80a055fbbe058a8f227e485b7d
Vansh17604 <vansh456patel@gmail.com>
2025-06-08 22:10:58 +0530

    Updert

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
 app.use('/uploads', express.static(path.resolve(__dirname, '../public/uploads')));
 
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../../frontend/dist/index.html'));
 });
 
